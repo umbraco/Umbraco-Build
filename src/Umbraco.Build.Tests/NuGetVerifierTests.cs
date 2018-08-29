4720bd26825e99917d7fd740da478a7fa46c86a7
Stephan <stephane@umbraco.dk>
2018-08-29 10:54:42 +0200

    Create some tests

---
+++ src/Umbraco.Build.Tests/NuGetVerifierTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Umbraco.Build.Tests
+{
+    [TestFixture]
+    public class NuGetVerifierTests
+    {
+        [Test]
+        public void Test()
+        {
+            var path = Assembly.GetExecutingAssembly().Location;
+            for (var i = 0; i < 5; i++)
+                path = Path.GetDirectoryName(path);
+
+            var verifier = new NuGetVerifier();
+            var deps = verifier.GetNuSpecDependencies(Path.Combine(path, "UmbracoCms.Web.nuspec"));
+        }
+    }
+}
