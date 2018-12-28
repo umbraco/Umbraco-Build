449002d125c6e237c838aa93fe26a376c0b53284
Stephan <stephane@umbraco.dk>
2018-12-28 21:26:59 +0100

    Version 0.2.0

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -13,8 +13,8 @@ using System.Resources;
 
 // this is the ONLY ONE the CLR cares about for compatibility
 // should change ONLY when "hard" breaking compatibility (manual change)
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
 
 // these are FYI and changed automatically
-[assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha.58")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
